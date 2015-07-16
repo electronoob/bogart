8e2238707ad54abbac7a13f9d7173a5b628d6f66
Electronoob <github@electronoob.com>
2015-07-17 00:48:20 +0100

    begun porting to three.js

--- script.js
+++ script.js
@@ -11,12 +11,6 @@ var clients = [], spec = null;
 var offsetX = 0, offsetY = 0;
 var scale_x = 4, scale_y = 4;
 
-var FEED_TARGETS = ["gc", "BotMaster", "white light", "white  light",
-                    "tubbymcfatfuck", "texas  doge", "white  light",
-                    "doge  helper", "lord kience", "drunken",
-                    "controless", "notreallyabot",
-                    "sqochit", "blueeyes",
-                    "skref is life", "shrek is love"];
 
 world.onleaderschange = function (new_leaders) {
   if ($leaderboard) {
@@ -68,127 +62,7 @@ function start() {
       scale_y += d; 
       console.log(d);
   };
-  /*
-  clients = [];
-  for (var i = 0; i < NUM_BOTS; ++i) {
-    setTimeout(function () {
-      var name = "BOT.." + + Math.floor(Math.random() * 1000);
-      clients.push(new AgarClient(name, world));
-      console.log("Launching " + name);
-    }, i * 1500);
-  }
 
-  if (inter != null) {clearInterval(inter);}
-
-  inter = setInterval(function () {
-    var vecs = [];
-    for (var i = 0; i < clients.length; ++i) {
-      vecs[i] = [0, 0];
-      clients[i].vecs = [];
-    }
-
-    var objects = world.objects;
-    for (var id in objects) {
-      if (objects.hasOwnProperty(id)) {
-        var o = objects[id], x = o.x, y = o.y, size = o.size;
-        if (o.isBot) { continue; }
-
-        for (var i = 0; i < clients.length; ++i) {
-          var c = clients[i];
-          if (c.id > -1 && id != c.id) {
-            var dx = c.x - x, dy = c.y - y;
-
-            if (Math.abs(dx) < 10000 && Math.abs(dy) < 10000) {
-              // viruses (when we're small) and other bots don't matter
-              if ((o.isVirus && c.size < 75) || o.isBot) { continue; }
-
-              var d = Math.sqrt(dx*dx + dy*dy);
-              var dir = (c.size < o.size || o.isVirus)? 1 : -1;
-              var cost = Math.abs(c.size - o.size);
-
-              if (o.size > c.size * 0.85 && c.size > o.size * 0.85) {
-                // another blob that we can't eat and it can't eat us
-                cost = 0;
-              } else if (o.size * 0.85 > c.size) {
-                // another blob large enough to hurt us (including
-                // viruses)
-                //
-                dir = 1;
-
-                // can they split kill us? virtually reduce the distance
-                var effective_d = d;
-                if ((o.size / 2) * 0.85 > c.size) {
-                  effective_d = Math.max(0, d-660);
-                }
-
-                cost = (effective_d - o.size)/o.size;
-                if (cost <= 0) { cost = 0.0000001; }
-                cost = Math.pow(1/cost, 2);
-              }
-
-              // Bots should join together, or sacrifice themselves
-              // for their human masters!
-              if (o.feed_target || o.isBot) {
-                dir = -1;
-                cost = 999999999;
-              }
-
-              // Food pellets
-              if (o.size < 14) {
-                dir = -1;
-                if (d < 250) {
-                  cost *= 250 - d;
-                }
-              }
-
-              var v = [dir * cost * dx / (d*d),
-                       dir * cost * dy / (d*d)];
-              vecs[i][0] += v[0];
-              vecs[i][1] += v[1];
-              c.vecs.push(v);
-            }
-          }
-        }
-      }
-    }
-
-    for (var i = 0; i < clients.length; ++i) {
-      var c = clients[i];
-      if (c.id != -1) {
-        var norm = Math.sqrt(vecs[i][0] * vecs[i][0] + vecs[i][1] * vecs[i][1]);
-        if (norm == 0) {
-          norm = 1;
-        }
-        var x = (vecs[i][0]/norm) * 10000;
-        var y = (vecs[i][1]/norm) * 10000;
-
-        if (x < 0 && c.x < c.size) {
-          y = 0;
-          y = Math.sign(y) * 10000;
-        }
-
-        if (x > 0 && c.x > world.width - c.size) {
-          x = 0;
-          y = Math.sign(y) * 10000;
-        }
-
-        if (y < 0 && c.y < c.size) {
-          y = 0;
-          x = Math.sign(x) * 10000;
-        }
-
-        if (y > 0 && c.y > world.height - c.size) {
-          y = 0;
-          x = Math.sign(x) * 10000;
-        }
-
-        clients[i].dx = x;
-        clients[i].dy = y;
-        clients[i].sendDirection();
-      }
-    }
-  }, 50);
- */
 }
 
 function drawArrow(ctx, x, y, dx, dy) {
